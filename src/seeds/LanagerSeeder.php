ad59900bf61756ea55effd324db774c2751d7241
ilumos <ilumos@gmail.com>
2013-11-12 22:28:14 +0000

    Updated database seed

--- src/seeds/LanagerSeeder.php
+++ src/seeds/LanagerSeeder.php
@@ -23,15 +23,22 @@ class InfoPageTableSeeder extends Seeder {
 
     public function run()
     {
-        // Empty before we seed
-        DB::table('info_pages')->delete();
+
+        DB::table('info_pages')->delete(); // Empty before we seed
+
+        $lanagerContent = 
+        "*What the hell is this?*\r\n\r\n"
+        ."This site is the LANager, available to you during your stay and designed to make life easier for everyone!\r\n\r\n"
+        ."**On the LANager you can:**\r\n\r\n"
+        ."* Find out useful stuff from the [Info](/info) section\r\n\r\n"
+        ."* ... more to come\r\n\r\n";
+
 
         InfoPage::create(array(
         	'title' => 'The LANager',
-			'content' =>
-				"### What the hell is this? ###\r\n\r\nSo, this site, the LANager, is available to you during your stay at the event.\r\n\r\nAt this stage there isn't much here, but as features are added they'll be summarised here."
-        ));
-
+			'content' => $lanagerContent
+			));
+				
     }
 
 }
