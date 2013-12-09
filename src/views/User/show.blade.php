54868c15f13635af777cfc0515ba997d084cd6ac
ilumos <ilumos@gmail.com>
2013-12-09 21:27:19 +0000

    Correcting referenced library method names

--- src/views/User/show.blade.php
+++ src/views/User/show.blade.php
@@ -4,8 +4,8 @@
 	<img class="user_show_avatar_large" src="{{ $steamUser->getLargeAvatarUrl() }}">
 	<span class="user_show_username">{{{ $user->username }}}</span>
 	<ul class="user_show_tools">
-		<li><a href="{{ SteamBrowserProtocol::getAddFriendLink($user->steam_id_64) }}">Add</a></li>
-		<li><a href="{{ SteamBrowserProtocol::getSendMessageLink($user->steam_id_64) }}">Message</a></li>
+		<li><a href="{{ SteamBrowserProtocol::addFriend($user->steam_id_64) }}">Add</a></li>
+		<li><a href="{{ SteamBrowserProtocol::messageFriend($user->steam_id_64) }}">Message</a></li>
 		<li><a href="http://www.steamcommunity.com/profiles/{{ $user->steam_id_64 }}" target="_blank">View Steam Profile</a></li>
 	</ul>
 </div>
