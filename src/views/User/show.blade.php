d5b1d714bcb6e785d39a333f4957b97e806e70d7
ilumos <ilumos@gmail.com>
2013-12-21 21:56:06 +0000

    Added current game & server display

--- src/views/User/show.blade.php
+++ src/views/User/show.blade.php
@@ -1,5 +1,6 @@
 @extends('lanager-core::layouts.default')
 @section('content')
+<?php $steamState = $user->steamStates()->latest(); ?>
 <div class="user_show_header">
 	<img class="user_show_avatar_large" src="{{ $user->getLargeAvatarUrl() }}">
 	<span class="user_show_username">{{{ $user->username }}}</span>
@@ -11,7 +12,17 @@
 </div>
 <div class="user_show_content">
 	<div class="user_show_status pull-right">
-		{{ 	$user->steamStates()->latest()->getStatus() }}
+		{{ $steamState->getStatus() }}
+		@if( is_numeric($steamState->app_id) )
+			:
+			<a href="{{ SteamBrowserProtocol::viewAppInStore($steamState->app_id) }}">
+				{{{ $steamState->app_name }}}<br>
+				<img src="http://cdn.steampowered.com/v/gfx/apps/{{ $steamState->app_id }}/capsule_184x69.jpg"></a>
+			<br>
+			@if( !empty($steamState->server_ip) )
+				{{ link_to(SteamBrowserProtocol::connectToServer($steamState->server_ip), 'Join') }}
+			@endif
+		@endif
 	</div>
 	<br>
 </div>
