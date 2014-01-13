e3379f593a656a820d1c44f2aefda835e5cbf12f
ilumos <ilumos@gmail.com>
2014-01-13 22:00:19 +0000

    Restyled shout page with bootstrap components

--- src/views/Shout/list.blade.php
+++ src/views/Shout/list.blade.php
@@ -3,24 +3,33 @@
 @else
 	@foreach ($shouts as $shout)
 		<?php $date = new ExpressiveDate($shout->created_at); ?>
-		<div class="shout-post-item">
-			<img src="{{ $shout->user->avatar }}" alt="Avatar">
-			{{ link_to_route('user.show', $shout->user->username, $shout->user->id) }}
-			@if( Authority::can('manage', 'shout'))
-				<div class="shout-post-moderation pull-right">
-					{{ Button::link(URL::route('shout.pin', array('shout' => $shout->id)), 'Pin') }}
-					{{ HTML::resourceDelete('shout', $shout->id, 'Delete') }}
+			<div class="media">
+				<a class="pull-left" href="{{ URL::route('user.show', $shout->user->id) }}">
+					<img class="media-object" src="{{ $shout->user->avatar }}" alt="Avatar">
+				</a>
+				@if( Authority::can('manage', 'shout'))
+					<div class="shout-moderation pull-right">
+						{{ Button::link(URL::route('shout.pin', array('shout' => $shout->id)), 'Pin') }}
+						{{ HTML::resourceDelete('shout', $shout->id, 'Delete') }}
+					</div>
+				@endif
+				<span class="pull-right shout-timestamp" title="{{ $date }}">
+					{{ $date->getRelativeDate() }}
+				</span>
+				@if ($shout->pinned)
+					<span class="glyphicon glyphicon-star pull-right" title="This post has been pinned"></span>
+				@endif
+				<div class="media-body shout-body">
+					<h4 class="media-heading">
+						{{ link_to_route('user.show', $shout->user->username, $shout->user->id) }}
+						@foreach($shout->user->roles as $role)
+							<span class="badge">{{ $role->name }}</span>
+						@endforeach
+					</h4>
+					{{{ $shout->content }}}
 				</div>
-			@endif
-			<span class="shout-post-time pull-right" title="{{ $date }}">
-				{{ $date->getRelativeDate() }}
-			</span>
-			@if ($shout->pinned)
-				<span class="glyphicon glyphicon-star-empty pull-right" title="This post has been pinned"></span>
-			@endif
-			<div class="shout-post-item-content">{{{ $shout->content }}}</div>
 
-		</div>
+			</div>
 	@endforeach
 	{{ $shouts->links() }}
 @endif
