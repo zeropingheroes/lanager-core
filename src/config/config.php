625a1f637525e1fb0498738cfa838d456e2afa0f
ilumos <ilumos@gmail.com>
2014-01-29 22:07:52 +0000

    Added new config settings

--- src/config/config.php
+++ src/config/config.php
@@ -49,4 +49,36 @@ return array(
 	),
 
 
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Profile
+	|--------------------------------------------------------------------------
+	|
+	| For user profiles you may configure how many of each type of user generated
+	| content gets displayed.
+	| 
+	|
+	*/
+	'userProfile' => array(
+		'shoutQuantity' => 3,
+	),
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Maximum State Age
+	|--------------------------------------------------------------------------
+	|
+	| Set the maximum age in seconds of a user state
+	| States that are older than this will not be considered "live" and thus
+	| not displayed on a user's profile or count towards the "currently in use"
+	| statistics pages.
+	| Be sure to set this higher than your schedule for collecting states
+	|
+	*/
+	'states' => array(
+		'maximumAge' => 300,
+	),
+
 );
