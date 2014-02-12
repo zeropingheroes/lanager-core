67f7729d32fc99e0bbdc1672db32217f279449b7
ilumos <ilumos@gmail.com>
2014-02-12 18:56:39 +0000

    Added markdown help link

--- src/views/InfoPage/form.blade.php
+++ src/views/InfoPage/form.blade.php
@@ -6,6 +6,8 @@
 {{ Form::label('content', 'Content') }}
 {{ Form::textarea('content',NULL,array('placeholder' => 'The content of the page, markdown formatting enabled', 'rows' => 10)) }}
 
+{{ Form::block_help('<a href="https://daringfireball.net/projects/markdown/basics" target="_blank">Markdown cheatsheet</a>') }}
+
 {{ Form::label('parent_id', 'Parent') }}
 {{ Form::select('parent_id', $infoPagesDropdown) }}
 
