d79c8c4741a68f61c4b4e040183cb0a1367bb047
ilumos <ilumos@gmail.com>
2014-02-16 00:11:09 +0000

    Condensed event list page

--- src/views/Event/index.blade.php
+++ src/views/Event/index.blade.php
@@ -3,17 +3,37 @@
 	<h2>{{{ $title }}}</h2>
 	@if(count($events))
 		{{ Table::open() }}
-		{{ Table::headers('Name', 'Starts', '', 'Ends', '', 'Type') }}
+		{{ Table::headers('Name', 'Time', 'Type', 'Signups') }}
 		<?php
 		foreach( $events as $event )
 		{
+			$eventTimespan = new Zeropingheroes\LanagerCore\Helpers\Timespan($event->start, $event->end);
+			if(isset($event->signup_opens))
+			{
+				$signupTimespan = new Zeropingheroes\LanagerCore\Helpers\Timespan($event->signup_opens, $event->signup_closes);
+				switch($signupTimespan->status)
+				{
+					case 0:
+						$signupTimespan->status = Label::info('Not Yet Open');
+						break;
+					case 1:
+						$signupTimespan->status = Label::success('Open');
+						break;
+					case 2:
+						$signupTimespan->status = Label::warning('Closed');
+						break;
+				}
+				$signups = $signupTimespan->status.' '.count($event->users).' '.str_plural('user',count($event->users)).' signed up';
+			}
+			else
+			{
+				$signups = '';
+			}
 			$tableBody[] = array(
 				'name'			=> link_to_route('event.show', $event->name, $event->id),
-				'start-day'		=> date('D jS', strtotime($event->start)),
-				'start-time'	=> date('g:ia', strtotime($event->start)),
-				'end-day'		=> date('D jS', strtotime($event->end)),
-				'end-time'		=> date('g:ia', strtotime($event->end)),
+				'time'			=> $eventTimespan->naturalFormat(),
 				'type'			=> (isset($event->event_type->name) ? $event->event_type->name : ''),
+				'signups'		=> $signups,
 			);
 		}
 		?>
